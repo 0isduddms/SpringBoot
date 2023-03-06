58c5b600cdaea27574289b5be9b01e7ae8318c40
0isduddms <dufmaqlw1@naver.com>
2023-03-06 21:35:29 +0900

    방명록의 조회 처리, 방명록의 수정 / 삭제 처리, 서비스 계층에서 수정과 삭제, POST 방식 수정처리, 수정 화면에서 이벤트 처리, 수정 화면에서 다시 목록 페이지로

--- src/main/java/com/example/demo/controller/GuestbookController.java
+++ src/main/java/com/example/demo/controller/GuestbookController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -52,12 +53,36 @@ public class GuestbookController {
         return "redirect:/guestbook/list";
     }
 
-    @GetMapping("/read")
+    //@GetMapping("/read")
+    @GetMapping({"/read", "/modify"})
     public void read(long gno, @ModelAttribute("requestDTO") PageRequestDTO requestDTO, Model model) {
         log.info("gno : " + gno);
 
         GuestbookDTO dto = service.read(gno);
 
         model.addAttribute("dto", dto);
+    }
+
+    @PostMapping("/remove")
+    public String remove(long gno, RedirectAttributes redirectAttributes) {
+        log.info("gno : " + gno);
+
+        service.remove(gno);
+
+        redirectAttributes.addFlashAttribute("msg", gno);
+        return "redirect:/guestbook/list";
+    }
+
+    @PostMapping("/modify")
+    public String modify(GuestbookDTO dto, @ModelAttribute("requestDTO") PageRequestDTO requestDTO, RedirectAttributes redirectAttributes){
+        log.info("post modify......................................");
+        log.info("dto : " + dto);
+
+        service.modify(dto);
+
+        redirectAttributes.addFlashAttribute("page", requestDTO.getPage());
+        redirectAttributes.addFlashAttribute("gno", dto.getGno());
+
+        return "redirect:/guestbook/read";
     }
 }
